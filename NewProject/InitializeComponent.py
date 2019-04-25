f8755672017d065669a4fa1c301f3bee1cff0887
abanoub abanoub <abanoub.goker@gmail.com>
2019-04-26 01:27:20 +0200

    1

---
+++ NewProject/InitializeComponent.py
@@ -0,0 +1,13 @@
+from tkinter import *
+def form(frm):
+
+    frm.update()
+    fw = frm.winfo_width()
+    fh = frm.winfo_height()
+    sw = frm.winfo_screenwidth()
+    sh = frm.winfo_screenheight()
+    x = (sw - fw) /2
+    y = (sh - fh) /2 -45
+
+    frm.title("statistics")
+    frm.geometry('%dx%d+%d+%d' % (fw,fh,x,y))
--- NewProject/Statistics.py
+++ NewProject/Statistics.py
@@ -1,35 +1,51 @@
 from tkinter import *
 import statistics
+import InitializeComponent as ic
+from collections import Counter
+import matplotlib.mlab as mlab
 import matplotlib.pyplot as plot
 
 font = ('tahoma',15)
 window = Tk()
-
 window.title("statistics")
-
 window.geometry('1000x500')
+ic.form(window)
 
 lbl = Label(window, text="Draw Histogram" , font = font)
 lbl.grid(column=0, row=0)
 txt = Entry(window, width=60 , font = font)
 txt.grid(column=1, row=0)
 
-def histo():
-
+def Sample():
     res = list(str(txt.get()).split(','))
     sample = []
-
     for a in res:
         sample.append(int(a))
-
-    print(statistics.mean(sample))
-    print(statistics.median(sample))
-
-    # plot.boxplot(sample)
-    plot.hist(sample)
-
+    return sample
+def frequancy():
+    map = dict(Counter(Sample()))
+    x = []
+    for val in map.values():
+        x.append(int(val))
+    return  x
+def Labels():
+    labels = []
+    for i in Sample():
+        if str(i) in labels:
+            continue
+        else:
+            labels.append(str(i))
+    return  labels
+def pieChart():
+    plot.pie(frequancy(), autopct='%1.1f%%', shadow=True, startangle=140, labels=Labels())
     plot.show()
 
-btn = Button(window, text="Show Histogram", command=histo, font = font)
-btn.grid(column=2, row=0)
+
+Button(window, text="Show Histogram", command=pieChart, font = font).grid(column=2, row=0)
+
+Button(window, text="Show Histogram", command=pieChart, font = font).grid(column=2, row=0)
+
+Button(window, text="Show Histogram", command=pieChart, font = font).grid(column=2, row=0)
+
+
 window.mainloop()
