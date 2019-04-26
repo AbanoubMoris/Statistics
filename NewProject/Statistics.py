22c5e7584d3484792b56c3b2606d36698e2ade96
abanoub abanoub <abanoub.goker@gmail.com>
2019-04-26 02:24:11 +0200

    1

--- NewProject/Statistics.py
+++ NewProject/Statistics.py
@@ -11,41 +11,64 @@ window.title("statistics")
 window.geometry('1000x500')
 ic.form(window)
 
-lbl = Label(window, text="Draw Histogram" , font = font)
-lbl.grid(column=0, row=0)
-txt = Entry(window, width=60 , font = font)
-txt.grid(column=1, row=0)
+Label(window, text="Variables" , font = font).grid(column=0, row=0)
+vartxt = Entry(window, width=60 , font = font)
+vartxt.grid(column=1, row=0)
+
+Label(window, text="frequancy  " , font = font).grid(column=0, row=1)
+freqTxt = Entry(window, width=60 , font = font)
+freqTxt.grid(column=1, row=1)
+
+Label(window, text="sample" , font = font).grid(column=0, row=2)
+sampleTxt = Entry(window, width=60 , font = font)
+sampleTxt.grid(column=1, row=2)
+
+
 
 def Sample():
-    res = list(str(txt.get()).split(','))
+    res = list(str(sampleTxt.get()).split(','))
     sample = []
     for a in res:
         sample.append(int(a))
     return sample
 def frequancy():
-    map = dict(Counter(Sample()))
-    x = []
-    for val in map.values():
-        x.append(int(val))
-    return  x
+    freq = []
+    if str(freqTxt.get()) == '':
+        map = dict(Counter(Sample()))
+        for val in map.values():
+            freq.append(int(val))
+    else:
+        res = list(str(freqTxt.get()).split(','))
+        for a in res:
+            freq.append(int(a))
+    return freq
+
 def Labels():
     labels = []
-    for i in Sample():
-        if str(i) in labels:
-            continue
-        else:
-            labels.append(str(i))
+    if str(vartxt.get())=='':
+        for i in Sample():
+            if str(i) in labels:
+                continue
+            else:
+                labels.append(str(i))
+    else:
+        labels = list(str(vartxt.get()).split(','))
     return  labels
+
 def pieChart():
     plot.pie(frequancy(), autopct='%1.1f%%', shadow=True, startangle=140, labels=Labels())
     plot.show()
 
 
-Button(window, text="Show Histogram", command=pieChart, font = font).grid(column=2, row=0)
+def rsma():
+    plot.bar(frequancy())
+    plot.show()
 
-Button(window, text="Show Histogram", command=pieChart, font = font).grid(column=2, row=0)
+Button(window, text="Show PieChart ", command=pieChart, font = font).grid(column=2, row=0)
 
-Button(window, text="Show Histogram", command=pieChart, font = font).grid(column=2, row=0)
+Button(window, text="Show rsma     ", command=rsma, font = font).grid(column=2, row=1)
+
+Button(window, text="Show Histogram", command=pieChart, font = font).grid(column=2, row=2)
 
 
 window.mainloop()
